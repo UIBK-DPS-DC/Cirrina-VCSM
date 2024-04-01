163903c2044533f97af196040764054eb2bca413
Lukas Eisenstecken <lukas.eisenstecken@student.uibk.ac.at>
2024-04-01 19:10:58 +0200

    extended and refactored guard classes

--- src/classes/guard/GuardClass.tsx
+++ src/classes/guard/GuardClass.tsx
@@ -1,11 +1,31 @@
 export class GuardClass {
-    expression: string;
-    guardName ? :string
+    private _expression: string;
+    private _guardName?: string | undefined;
 
-    constructor(expression: string, guardName: string ){
-        this.expression = expression;
-        this.guardName = guardName;
+    constructor();
+    constructor(expression: string, guardName:string);
+
+    constructor(expression?: string, guardName?: string ){
+        this._expression = expression || "";
+        this._guardName = guardName || undefined;
         
     }
+
+    public get expression(): string {
+        return this._expression;
+    }
+    public set expression(value: string) {
+        this._expression = value;
+    }
+
+    public get guardName(): string | undefined {
+        return this._guardName;
+    }
+    public set guardName(value: string | undefined) {
+        this._guardName = value;
+    }
+
+
+
 
 }
