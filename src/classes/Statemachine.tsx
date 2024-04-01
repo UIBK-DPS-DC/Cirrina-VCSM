4824d5eb20ef2c0c4b22dde371ef8f8ee9fab53e
Lukas Eisenstecken <lukas.eisenstecken@student.uibk.ac.at>
2024-04-01 20:01:31 +0200

    finished up statemachine class

--- src/classes/Statemachine.tsx
+++ src/classes/Statemachine.tsx
@@ -1,18 +1,90 @@
-import { StateOrStatemachineClass } from "./Interfaces.tsx";
+import { ActionOrActionReferenceClass, StateOrStatemachineClass } from "./Interfaces.tsx";
 import { ContextClass } from "./context/ContextClass.tsx";
 import { GuardClass } from "./guard/GuardClass.tsx";
 
 class Statemachine implements StateOrStatemachineClass {
-   name : string;
-   states : Array<StateOrStatemachineClass>;
-   localContext?: ContextClass;
-   presistentContext?: ContextClass;
-   guards?: Array<GuardClass>
-   inherit?: string
-   isAbstract?: boolean
-   
+   private _name: string;
+   private _states: Array<StateOrStatemachineClass>;
+   private _localContext?: ContextClass | undefined;
+   private _persistentContext?: ContextClass | undefined;
+   private _guards?: Array<GuardClass> | undefined;
+   private _actions?: Array<ActionOrActionReferenceClass> | undefined;
+   private _inherit?: string | undefined;
+   private _isAbstract?: boolean | undefined;
 
 
+
+   // We should construct these using a builder, however feel free to add more constructors as needed
+   constructor();
+   constructor(name: string)
+   constructor(name?: string){
+      this._name = name || ""
+      this._states = new Array<StateOrStatemachineClass>()
+      this._localContext = undefined
+      this._persistentContext = undefined
+      this._guards = undefined
+      this._actions = undefined
+      this._inherit = undefined;
+      this._isAbstract = undefined;
+
+
+   }
+
+   public get name(): string {
+      return this._name;
+   }
+   public set name(value: string) {
+      this._name = value;
+   }
+
+   public get states(): Array<StateOrStatemachineClass> {
+      return this._states;
+   }
+   public set states(value: Array<StateOrStatemachineClass>) {
+      this._states = value;
+   }
+
+   public get localContext(): ContextClass | undefined {
+      return this._localContext;
+   }
+   public set localContext(value: ContextClass | undefined) {
+      this._localContext = value;
+   }
+
+   public get persistentContext(): ContextClass | undefined {
+      return this._persistentContext;
+   }
+   public set persistentContext(value: ContextClass | undefined) {
+      this._persistentContext = value;
+   }
+
+   public get guards(): Array<GuardClass> | undefined {
+      return this._guards;
+   }
+   public set guards(value: Array<GuardClass> | undefined) {
+      this._guards = value;
+   }
+
+   public get actions(): Array<ActionOrActionReferenceClass> | undefined {
+      return this._actions;
+   }
+   public set actions(value: Array<ActionOrActionReferenceClass> | undefined) {
+      this._actions = value;
+   }
+
+   public get inherit(): string | undefined {
+      return this._inherit;
+   }
+   public set inherit(value: string | undefined) {
+      this._inherit = value;
+   }
+
+   public get isAbstract(): boolean | undefined {
+      return this._isAbstract;
+   }
+   public set isAbstract(value: boolean | undefined) {
+      this._isAbstract = value;
+   }
 
 
 
