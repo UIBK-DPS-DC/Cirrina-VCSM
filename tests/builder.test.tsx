093cabe05bbf9d459be8922cac0058e18d8053f1
Lukas Eisenstecken <lukas.eisenstecken@student.uibk.ac.at>
2024-04-02 19:26:54 +0200

    expanded builder, added tests for reset function

--- tests/builder.test.tsx
+++ tests/builder.test.tsx
@@ -1,9 +1,22 @@
 import {StateBuilder} from "../src/classes/builders/StateBuilder"
 import {StateClass} from "../src/classes/StateClass"
+
 test("The statebuilder's build function should return undefined if no name is set", () => {
     const stateBuilder: StateBuilder = new StateBuilder();
-    const State: StateClass | undefined = stateBuilder.build();
+    const state: StateClass | undefined = stateBuilder.build();
 
-    expect(State).toBeUndefined();
+    expect(state).toBeUndefined();
 
 });
+
+test("The statebuilder's reset function should reset the internal state to a new state object", () => {
+    const stateBuilder: StateBuilder = new StateBuilder();
+    const stateName: string = "stateName";
+    const state1: StateClass | undefined = stateBuilder.setName(stateName).build();
+
+    stateBuilder.reset();
+    const state2: StateClass | undefined = stateBuilder.setName(stateName).build();
+
+    expect(state1 === state2).toBeFalsy()
+    
+})
