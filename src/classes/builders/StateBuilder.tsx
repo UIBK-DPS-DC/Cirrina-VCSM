2fd1640eb54c175a561a2687aca8a115ab1a7354
Lukas Eisenstecken <lukas.eisenstecken@student.uibk.ac.at>
2024-04-02 22:19:21 +0200

    more tests for StateBuilder

--- src/classes/builders/StateBuilder.tsx
+++ src/classes/builders/StateBuilder.tsx
@@ -1,5 +1,7 @@
-import { Builder } from "../Interfaces";
+import { ActionOrActionReferenceClass, Builder } from "../Interfaces";
 import { StateClass } from "../StateClass";
+import { ContextClass } from "../context/ContextClass";
+import { OnTransitionClass } from "../transition/OnTransitionClass";
 
 export class StateBuilder implements Builder<StateClass>{
     private _state : StateClass;
@@ -32,6 +34,61 @@ export class StateBuilder implements Builder<StateClass>{
         this._state.name = name;
         return this;
     }
+
+    public setIsInitial(value: boolean): StateBuilder{
+        this._state.isInitial = value;
+        return this;
+    }
+
+    public setIsTerminal(value: boolean): StateBuilder{
+        this._state.isTerminal = value;
+        return this
+    }
+
+    
+    public setEntryActions(actions: Array<ActionOrActionReferenceClass>) : StateBuilder{
+        this._state.entry = actions;
+        return this;
+    }
+
+    public setOnEventTransitions(transitions: Array<OnTransitionClass>): StateBuilder{
+        this._state.on = transitions;
+        return this;
+    }
+
+    public setAlwaysTransitions(transitions : Array<OnTransitionClass>): StateBuilder{
+        this._state.always = transitions;
+        return this;
+    }
+
+    public setLocalContext(context: ContextClass): StateBuilder{
+        this._state.localContext = context;
+        return this;
+    }
+
+    public setPersistentContext(context: ContextClass): StateBuilder{
+        this._state.persistentContext = context;
+        return this;
+    }
+
+    public setStaticContext(context: ContextClass): StateBuilder {
+        this._state.staticContext = context;
+        return this;
+    }
+
+    public setIsVirtual(value: boolean): StateBuilder{
+        this._state.isVirtual = value;
+        return this;
+    }
+
+    public setIsAbstract(value: boolean): StateBuilder{
+        this._state.isAbstract = value;
+        return this;
+    }
+    
+
+
+
 
 
 
--- tests/builder.test.tsx
+++ tests/builder.test.tsx
@@ -1,16 +1,19 @@
 import {StateBuilder} from "../src/classes/builders/StateBuilder"
 import {StateClass} from "../src/classes/StateClass"
 
-test("The statebuilder's build function should return undefined if no name is set", () => {
-    const stateBuilder: StateBuilder = new StateBuilder();
-    const state: StateClass | undefined = stateBuilder.build();
+let stateBuilder = new StateBuilder();
 
+beforeEach(() => {
+    stateBuilder.reset();
+  });
+
+test("The statebuilder's build function should return undefined if no name is set", () => {
+    const state: StateClass | undefined = stateBuilder.build();
     expect(state).toBeUndefined();
 
 });
 
 test("The statebuilder's reset function should reset the internal state to a new state object", () => {
-    const stateBuilder: StateBuilder = new StateBuilder();
     const stateName: string = "stateName";
     const state1: StateClass | undefined = stateBuilder.setName(stateName).build();
 
@@ -20,3 +23,31 @@ test("The statebuilder's reset function should reset the internal state to a new
     expect(state1 === state2).toBeFalsy()
     
 })
+
+test("The statebuilders set function for isTerminal and isInitial should initialize the properties correctly", () => {
+    let isInitial = true;
+    let isTerminal = false;
+
+    let state = stateBuilder.
+    setIsInitial(isInitial)
+    .setIsTerminal(isTerminal)
+    .build();
+
+    expect(state?.isInitial == isInitial);
+    expect(state?.isTerminal == isTerminal);
+
+    isInitial = false;
+    isTerminal = true;
+
+    stateBuilder.reset();
+    state = stateBuilder
+    .setIsInitial(isInitial)
+    .setIsTerminal(isTerminal)
+    .build();
+
+    expect(state?.isInitial == isInitial);
+    expect(state?.isTerminal == isTerminal);
+    
+}
+
+)
