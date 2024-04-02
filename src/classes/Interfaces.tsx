ceda9c843d15e2558ea6b90a07637986a60cad2d
Lukas Eisenstecken <lukas.eisenstecken@student.uibk.ac.at>
2024-04-02 23:19:22 +0200

    docs

--- src/classes/Interfaces.tsx
+++ src/classes/Interfaces.tsx
@@ -12,5 +12,7 @@ export interface GuardOrGuardReferenceClass {
 
 export interface Builder<T>{
     build(): T | undefined;
+    
+    /* Should reset the builder by assigning it a new Object of type T */
     reset(): void;
 }
