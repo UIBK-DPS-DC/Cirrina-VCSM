b2f08c26b72ec28188f6b624e6e9fa58235a2424
Lukas Eisenstecken <lukas.eisenstecken@student.uibk.ac.at>
2024-04-03 17:08:44 +0200

    implemented statemachine builder

---
+++ src/classes/builders/StatemachineBuilder.tsx
@@ -0,0 +1,61 @@
+import { ActionOrActionReferenceClass, Builder, StateOrStatemachineClass } from "../Interfaces";
+import { StatemachineClass } from "../StatemachineClass";
+import { GuardClass } from "../guard/GuardClass";
+
+export class StatemachineBuilder implements Builder<StatemachineClass>{
+    private _statemachine: StatemachineClass;
+
+    constructor(){  
+        this._statemachine = new StatemachineClass();
+    }
+
+    public build(): StatemachineClass | undefined {
+        if(this._statemachine.name == "" || this._statemachine.states.length < 1){
+            console.log("Statemachine must have a name and at least one state");
+            return undefined;
+        }
+        else{
+            return this._statemachine;
+        }
+    }
+
+    public reset(): void {
+        this._statemachine = new StatemachineClass();
+    }
+
+    public setName(name: string): StatemachineBuilder {
+        this._statemachine.name = name;
+        return this;
+    }
+
+    public setStates(states: Array<StateOrStatemachineClass>): StatemachineBuilder{
+        this._statemachine.states = states;
+        return this;
+    }
+
+    public setGuards(guards: Array<GuardClass>): StatemachineBuilder{
+        this._statemachine.guards = guards;
+        return this;
+    }
+
+    public setActions(actions: Array<ActionOrActionReferenceClass>): StatemachineBuilder {
+        this._statemachine.actions = actions;
+        return this;
+    }
+
+    public setInherit(statemachineName: string): StatemachineBuilder{
+        this._statemachine.inherit = statemachineName;
+        return this;
+    }
+
+    public setIsAbstract(value: boolean): StatemachineBuilder {
+        this._statemachine.isAbstract = value;
+        return this;
+    }
+
+    
+
+
+
+
+}
