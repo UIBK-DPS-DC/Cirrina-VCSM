a05edd99de987d140f2763f4cd9ec3d6952c2d46
Lukas Eisenstecken <lukas.eisenstecken@student.uibk.ac.at>
2024-04-03 18:55:58 +0200

    even more tests

--- test/StatemachineBuilder.test.tsx
+++ test/StatemachineBuilder.test.tsx
@@ -2,9 +2,11 @@ import {StatemachineBuilder} from "../src/classes/builders/StatemachineBuilder"
 import { StateClass } from "../src/classes/StateClass";
 import { StateBuilder } from "../src/classes/builders/StateBuilder";
 import {expect, test, beforeEach} from '@jest/globals';
-import { StateOrStatemachineClass } from "../src/classes/Interfaces";
+import { ActionOrActionReferenceClass, StateOrStatemachineClass } from "../src/classes/Interfaces";
 import { StatemachineClass } from "../src/classes/StatemachineClass";
 import { ContextClass } from "../src/classes/context/ContextClass";
+import { GuardClass } from "../src/classes/guard/GuardClass";
+import { ActionReferenceClass } from "../src/classes/action/ActionReferenceClass";
 
 const statemachineBuilder = new StatemachineBuilder();
 const stateBuilder = new StateBuilder();
@@ -15,6 +17,7 @@ const states: Array<StateOrStatemachineClass> = new Array<StateOrStatemachineCla
 beforeEach(() => {
     statemachineBuilder.reset()
     stateBuilder.reset()
+    stateBuilder.setName(stateMachineName);
     statemachineBuilder
     .setName(stateMachineName)
     .setStates(states)
@@ -88,6 +91,68 @@ test("The Statemachine builder should set the local and persistent context corre
 
 
 }))
+
+test("The Statemachine builder should set the guards and actions correctly", () => {
+    const guardExpression = "expression";
+    const guard1 = new GuardClass(guardExpression);
+    const guard2 = new GuardClass(guardExpression);
+    const guards: Array<GuardClass> = new Array<GuardClass>();
+    guards.push(guard1);
+    guards.push(guard2);
+
+    const referenceName: string = "name";
+    const action1: ActionReferenceClass = new ActionReferenceClass(referenceName);
+    const action2: ActionReferenceClass = new ActionReferenceClass(referenceName);
+    const action3: ActionReferenceClass = new ActionReferenceClass(referenceName);
+
+    const actions: Array<ActionOrActionReferenceClass> = new Array<ActionOrActionReferenceClass>();
+    actions.push(action1);
+    actions.push(action2);
+    actions.push(action3);
+
+    const stateMachine: StatemachineClass = statemachineBuilder
+    .setGuards(guards)
+    .setActions(actions)
+    .build();
+
+    guards.forEach(
+        (x) => {
+            expect(stateMachine.guards).toContain(x);
+        }
+    )
+
+    actions.forEach(
+        (x) => {
+          expect(stateMachine.actions).toContain(x);  
+        }
+    )
+})
+
+
+test("The statemachine builder should set the inherit property correctly", () => {
+    const inheritedMachineName: string = "name";
+    const stateMachine: StatemachineClass = statemachineBuilder.setInherit(inheritedMachineName).build();
+
+    expect(stateMachine.inherit).toBe(inheritedMachineName);
+})
+
+test("The statemachine should set the isAbstract value correctly", () => {
+    let value: boolean = true;
+    let stateMachine: StatemachineClass = statemachineBuilder.setIsAbstract(value).build();
+
+    expect(stateMachine.isAbstract).toBe(value);
+
+    value = false;
+    stateBuilder.reset();
+
+    stateMachine = statemachineBuilder.setIsAbstract(value).build();
+    expect(stateMachine.isAbstract).toBe(value);
+
+
+
+
+
+})
 
 
 
