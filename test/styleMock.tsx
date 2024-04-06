ea3bd4082a1afefe8b7187bd15e89fbc6004133a
Lukas Eisenstecken <lukas.eisenstecken@student.uibk.ac.at>
2024-04-06 18:09:31 +0200

    we cooking bois

---
+++ test/styleMock.tsx
@@ -0,0 +1,3 @@
+// This exists to avoid the unexpected token error when importing css into jest tests.
+// See : https://stackoverflow.com/questions/54627028/jest-unexpected-token-when-importing-css
+module.exports = {};
