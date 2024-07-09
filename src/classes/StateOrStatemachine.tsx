f3fc892923badb0c1f10d8577599e0c0fc20a335
Lukas Eisenstecken <lukas.eisenstecken@student.uibk.ac.at>
2024-07-09 21:52:11 +0200

    Now renders basic statemachine nodes

--- src/classes/StateOrStatemachine.tsx
+++ src/classes/StateOrStatemachine.tsx
@@ -1,8 +1,10 @@
 export default class StateOrStatemachine {
     private static stateOrStatemachineNames: Set<string> = new Set()
+    private static idCount: number = 0
 
 
-    public static registerName(name: string): boolean  {
+
+    public static registerName(name: string): boolean {
         if(StateOrStatemachine.stateOrStatemachineNames.has(name)){
             console.log(name + " already exists");
             return false
@@ -10,6 +12,16 @@ export default class StateOrStatemachine {
 
         StateOrStatemachine.stateOrStatemachineNames.add(name)
         return true
+    }
+
+    public static getCurrentIdCount(): number {
+        return StateOrStatemachine.idCount
+    }
+
+    public static getNewId(){
+        const id = StateOrStatemachine.idCount
+        StateOrStatemachine.idCount+= 1
+        return id
     }
 
 
---
+++ src/types.tsx
@@ -0,0 +1,11 @@
+import { Node } from "@xyflow/react"
+import StateMachine from "./classes/statemachine"
+
+export type StateMachineProps = Node<{
+    stateMachine: StateMachine
+}, "stateMachine">
+
+
+
+
+
