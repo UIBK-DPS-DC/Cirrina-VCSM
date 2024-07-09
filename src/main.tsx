d1dfb420e483d0ca0028acc927c8fe264d3f19a8
Lukas Eisenstecken <lukas.eisenstecken@student.uibk.ac.at>
2024-07-09 17:51:13 +0200

    teardown

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Header from './Header'
-import './index.css'
-import Flow from './NodeFlow'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <>
-    <Flow></Flow>
-    
-    
-    
-    </>
-    
-  </React.StrictMode>,
-)
