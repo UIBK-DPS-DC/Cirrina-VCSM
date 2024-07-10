17423f61d7f55d36853b509e2fb6884b4e1882ad
Lukas Eisenstecken <lukas.eisenstecken@student.uibk.ac.at>
2024-07-10 13:23:37 +0200

    extended functionality to ensure unique names among statemachines, implemented tests

---
+++ src/builders/stateMachineBuilder.tsx
@@ -0,0 +1,29 @@
+import StateMachine from "../classes/statemachine";
+import { Builder } from "../interfaces";
+
+export default class StateMachineBuilder implements Builder<StateMachine> {
+    private stateMachine: StateMachine
+
+    public constructor(){
+        this.stateMachine = new StateMachine("");
+    }
+
+    public build(): StateMachine {
+        return this.stateMachine
+    }
+
+    public reset(): void {
+        this.stateMachine = new StateMachine("")
+    }
+
+    public validate(): boolean {
+        return this.stateMachine.getName() != ""
+    }
+
+    
+
+
+    
+    
+
+}
