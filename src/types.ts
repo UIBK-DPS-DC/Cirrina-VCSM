a9cbd61ff775f6aa3ee5d3232b807582da7492c7
Lukas Eisenstecken <lukas.eisenstecken@student.uibk.ac.at>
2024-07-13 18:57:57 +0200

    small fixes

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,6 @@
 import {Edge, Node} from "@xyflow/react";
 import Transition from "./classes/transition.ts";
+import StateMachine from "./classes/stateMachine.ts";
 
 export type NodeType =
     | 'entry-node'
@@ -23,6 +24,12 @@ export type ExitNode = Node<{
     name: string
 }, 'exit-node'>;
 
+
+// Node Types with classes. Unsure if needed
 export type CsmEdgeProps = Edge<{
     transition: Transition
 }, 'csmEdge'>;
+
+export type StateMachineProps = Node<{
+    stateMachine: StateMachine
+}>
