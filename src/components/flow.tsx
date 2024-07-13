72528aeae36de01197794c218ecc56456c0dad64
Lukas Eisenstecken <lukas.eisenstecken@student.uibk.ac.at>
2024-07-13 18:57:57 +0200

    small fixes

--- src/components/flow.tsx
+++ src/components/flow.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, createContext, useContext } from 'react';
+import React, { useCallback, createContext } from 'react';
 import {
     ReactFlow,
     Background,
--- src/components/stateMachineNodeComponent.tsx
+++ src/components/stateMachineNodeComponent.tsx
@@ -7,17 +7,17 @@ export default function StateMachineNode({data}: NodeProps<StateMachineProps>){
         <div className='state-machine-node'>
             <div>
                 <Handle
-                id = {data.stateMachine.getName()} // TODO: This needs to updateable on the fly. Use React State hooks
+                id = {data.stateMachine.name} // TODO: This needs to updateable on the fly. Use React State hooks
                 type='target'
                 position= {Position.Top}
                 isConnectable = {true}/>
             </div>
 
-            <p>{data.stateMachine.getName()}</p>
+            <p>{data.stateMachine.name}</p>
 
             <div>
                 <Handle
-                id = {data.stateMachine.getName()}
+                id = {data.stateMachine.name}
                 type= "source"
                 position= {Position.Bottom}
                 isConnectable= {true}/>
