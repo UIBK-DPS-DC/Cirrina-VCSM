349be056bdf2cc24c2599231fb35dbd8f4594cb6
Lukas Geisler csaz8605 <Lukas.Geisler@student.uibk.ac.at>
2024-07-14 18:54:09 +0200

    chore: fixed code smell
    Signed-off-by: Lukas Eisenstecken <lukas.eisenstecken.bz@gmail.com>

--- src/components/flow.tsx
+++ src/components/flow.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, createContext, useMemo, useState } from 'react';
+import React, {useCallback, createContext, useMemo, useState} from 'react';
 import {
     ReactFlow,
     Background,
@@ -7,22 +7,22 @@ import {
     addEdge,
     useNodesState,
     useEdgesState,
-    type OnConnect, type NodeTypes, useReactFlow, Edge, Node, Connection
+    type OnConnect, type NodeTypes, useReactFlow, Edge, Node
 } from '@xyflow/react';
 
 import '@xyflow/react/dist/style.css';
-import { EntryNode } from "./Nodes/entryNode.tsx";
-import { ExitNode } from "./Nodes/exitNode.tsx";
-import { StateNode } from "./Nodes/stateNode.tsx";
-import { StateMachineNode } from "./Nodes/stateMachineNode.tsx";
+import {EntryNode} from "./Nodes/entryNode.tsx";
+import {ExitNode} from "./Nodes/exitNode.tsx";
+import {StateNode} from "./Nodes/stateNode.tsx";
+import {StateMachineNode} from "./Nodes/stateMachineNode.tsx";
 import StateOrStateMachineService from "../services/stateOrStateMachineService.tsx";
-import {CsmEdgeProps, CsmNodeProps, ReactFlowContextProps} from "../types.ts";
+import {CsmNodeProps, isState, isStateMachine} from "../types.ts";
+
 
 import "../css/nodeForm.css"
 import StateMachine from "../classes/stateMachine.ts";
 import State from "../classes/state.ts";
-import NodeInfoForm from "./nodeInfoForm.tsx";
-import CsmEdge from "./csmEdgeComponent.tsx";
+
 
 const nodeTypes = {
     'entry-node': EntryNode,
@@ -31,60 +31,49 @@ const nodeTypes = {
     'state-machine-node': StateMachineNode,
 } satisfies NodeTypes;
 
-const edgeTypes = {
-    'csm-edge': CsmEdge,
-}
-
 const initialNodes: Node<CsmNodeProps>[] = [];
-const initialEdges: Edge<CsmEdgeProps>[] = [];
+const initialEdges: Edge[] = [];
 
-export const ReactFlowContext = createContext({});
+const ReactFlowContext = createContext({});
 
-let nodeId = 0;
-let edgeId = 0
-const getNewNodeId = () => `node_${nodeId++}`;
-const getNewEdgeId = () => `edge_${edgeId++}`;
+let id = 0;
+const getNewId = () => `node_${id++}`;
 
 export default function Flow() {
 
-    const stateOrStateMachineService: StateOrStateMachineService = useMemo(() => new StateOrStateMachineService(), []);
+    const stateOrStateMachineService: StateOrStateMachineService = useMemo( () => new StateOrStateMachineService(), []);
 
-    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [nodes, setNodes , onNodesChange] = useNodesState(initialNodes);
     const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
     const [selectedNode, setSelectedNode] = useState<Node<CsmNodeProps> | null>(null)
     const [showSidebar, setShowSidebar] = useState(false);
-    const [nameInput, setNameInput] = useState<string>("");
-    const { getIntersectingNodes, screenToFlowPosition } = useReactFlow();
+    const {getIntersectingNodes, screenToFlowPosition } = useReactFlow();
 
-    const contextValue: ReactFlowContextProps = {
+
+
+
+
+    const contextValue = {
         nodes,
         setNodes,
         edges,
         setEdges,
-        selectedNode,
-        setSelectedNode,
-        showSidebar,
-        setShowSidebar,
-        nameInput,
-        setNameInput,
-        stateOrStateMachineService
+        stateOrStateMachineService,
     }
 
+
     const onConnect: OnConnect = useCallback(
-        (connection: Connection) => {
-            const edge: Edge<CsmEdgeProps> = {id: getNewEdgeId(), ...connection, type: 'csm-edge' };
-            setEdges((eds) => addEdge(edge, eds));
-        },
-        [setEdges],
+        (connection) => setEdges((edges) => addEdge(connection, edges)),
+        [setEdges]
     );
 
-    const onDragOver = useCallback((event: React.DragEvent<HTMLDivElement>) => {
+    const onDragOver = useCallback((event:React.DragEvent<HTMLDivElement>) => {
         event.preventDefault();
         event.dataTransfer.dropEffect = 'move';
     }, []);
 
     const onDrop = useCallback(
-        (event: React.DragEvent<HTMLDivElement>) => {
+        (event:React.DragEvent<HTMLDivElement>) => {
             event.preventDefault();
 
             const type = event.dataTransfer.getData('application/reactflow');
@@ -98,42 +87,34 @@ export default function Flow() {
                 y: event.clientY,
             });
 
-            const new_name: string = stateOrStateMachineService.generateUniqueName(type);
-            stateOrStateMachineService.registerName(new_name);
+            const new_name: string  = stateOrStateMachineService.generateUniqueName(type)
+            stateOrStateMachineService.registerName(new_name)
 
-            const newNode: Node<CsmNodeProps> = {
-                id: getNewNodeId(),
+            const newNode : Node<CsmNodeProps> = {
+                id: getNewId(),
                 type,
                 position,
                 data: stateOrStateMachineService.getDefaultData(type, new_name),
             };
 
+
+
             // TODO add this to stylesheet
-            if (type === 'state-machine-node') {
+            if(type === 'state-machine-node') {
                 newNode.style = {
                     background: 'transparent',
-                    fontSize: 12,
-                    border: '1px solid black',
-                    padding: 5,
-                    borderRadius: 15,
-                    height: 150,
-                };
+                        fontSize: 12,
+                        border: '1px solid black',
+                        padding: 5,
+                        borderRadius: 15,
+                        height: 100,
+                }
             }
 
-            console.log(`Created ${newNode.id}`);
-
-            setNodes((nds) => {
-                // Order is important for nesting to work. See https://reactflow.dev/learn/layouting/sub-flows
-                if (type === 'state-machine-node') {
-                    return [newNode, ...nds]; // Prepend state-machine-nodes to the beginning.
-                } else {
-                    return [...nds, newNode]; // Append other nodes to the end
-                }
-            });
+            setNodes((nds) => nds.concat(newNode));
         },
         [screenToFlowPosition, setNodes, stateOrStateMachineService],
     );
-
 
     const onNodeDragStop = useCallback(
         (_: React.MouseEvent, node: Node) => {
@@ -141,7 +122,6 @@ export default function Flow() {
             const intersectedBlock = intersections.findLast(
                 (n) => n.type === "state-machine-node"
             );
-
             if (intersectedBlock) {
                 setNodes((ns) =>
                     ns.map((n) => {
@@ -170,7 +150,7 @@ export default function Flow() {
     );
 
     const onNodesDelete = useCallback(
-        (deletedNodes: Node[]) => {
+        (deletedNodes: Node []) => {
             deletedNodes.map(
                 (node) => {
                     switch (node.type) {
@@ -185,19 +165,23 @@ export default function Flow() {
                     }
                 }
             )
-        }, [stateOrStateMachineService]
+        },[stateOrStateMachineService]
     )
 
     const onNodeClick =
         useCallback((_: React.MouseEvent, node: Node<CsmNodeProps>) => {
             setSelectedNode(node);
             setShowSidebar(true);
-        }, [])
+        },[])
 
     const onPaneClick =
         useCallback(() => {
             setShowSidebar(false);
-        }, [])
+        },[])
+
+
+
+
 
     return (
         <ReactFlowContext.Provider value={contextValue}>
@@ -207,7 +191,6 @@ export default function Flow() {
                 onNodesChange={onNodesChange}
                 edges={edges}
                 onEdgesChange={onEdgesChange}
-                edgeTypes={edgeTypes}
                 onConnect={onConnect}
                 onPaneClick={onPaneClick}
                 onNodeClick={onNodeClick}
@@ -221,7 +204,22 @@ export default function Flow() {
                 <MiniMap />
                 <Controls />
             </ReactFlow>
-            <NodeInfoForm></NodeInfoForm>
+            {showSidebar && selectedNode && (
+                <div className = "node-form">
+                    <form>
+                        <h3>Hi mom! It's me {
+                            isState(selectedNode.data) ? selectedNode.data.state.name :
+                                isStateMachine(selectedNode.data) ? selectedNode.data.stateMachine.name :
+                                    selectedNode.data.name
+                        }!</h3>
+                    </form>
+
+                </div>
+            )
+
+            }
+
+
         </ReactFlowContext.Provider>
     );
 }
