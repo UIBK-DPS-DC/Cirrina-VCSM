34bf9d316ed8e8b1f4db06de8e8d0c035844ff29
Lukas Eisenstecken <lukas.eisenstecken@student.uibk.ac.at>
2024-07-14 18:40:19 +0200

    comments
    Signed-off-by: Lukas Eisenstecken <lukas.eisenstecken.bz@gmail.com>

--- src/types.ts
+++ src/types.ts
@@ -44,14 +44,42 @@ export type StateMachineProps = Node<{
 // Guards
 
 // Type guards
+
+/**
+ * Type guard function to check if the provided data is of type `{ state: State }`.
+ *
+ * This function takes a `CsmNodeProps` object and checks if it has a `state` property.
+ * It returns `true` if the `state` property is defined, indicating that the data is of type `{ state: State }`.
+ *
+ * @param {CsmNodeProps} data - The data to check.
+ * @returns {data is { state: State }} - Returns `true` if the data is of type `{ state: State }`.
+ */
 export function isState(data: CsmNodeProps): data is { state: State } {
     return (data as { state: State }).state !== undefined;
 }
 
+/**
+ * Type guard function to check if the provided data is of type `{ stateMachine: StateMachine }`.
+ *
+ * This function takes a `CsmNodeProps` object and checks if it has a `stateMachine` property.
+ * It returns `true` if the `stateMachine` property is defined, indicating that the data is of type `{ stateMachine: StateMachine }`.
+ *
+ * @param {CsmNodeProps} data - The data to check.
+ * @returns {data is { stateMachine: StateMachine }} - Returns `true` if the data is of type `{ stateMachine: StateMachine }`.
+ */
 export function isStateMachine(data: CsmNodeProps): data is { stateMachine: StateMachine } {
     return (data as { stateMachine: StateMachine }).stateMachine !== undefined;
 }
 
+/**
+ * Type guard function to check if the provided data is of type `{ name: string }`.
+ *
+ * This function takes a `CsmNodeProps` object and checks if it has a `name` property.
+ * It returns `true` if the `name` property is defined, indicating that the data is of type `{ name: string }`.
+ *
+ * @param {CsmNodeProps} data - The data to check.
+ * @returns {data is { name: string }} - Returns `true` if the data is of type `{ name: string }`.
+ */
 export function isExitOrEntry(data: CsmNodeProps): data is {name: string}{
     return (data as {name: string}).name !== undefined;
 }
