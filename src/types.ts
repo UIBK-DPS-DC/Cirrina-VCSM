caf20f98d8084ff72480ba2f558dde3505aaaac0
Lukas Eisenstecken <lukas.eisenstecken@student.uibk.ac.at>
2024-07-14 17:52:39 +0200

    basic sidebar implementation

--- src/types.ts
+++ src/types.ts
@@ -2,27 +2,24 @@ import {Edge, Node} from "@xyflow/react";
 import Transition from "./classes/transition.ts";
 import StateMachine from "./classes/stateMachine.ts";
 
+// One Type to avoid repeating ourselves. Can be expanded/unionized as needed.
+export type CsmNodeProps = {
+    name: string
+}
+
 export type NodeType =
     | 'entry-node'
     | 'exit-node'
     | 'state-node'
     | 'state-machine-node'
 
-export type EntryNode = Node<{
-    name: string
-}, 'entry-node'>;
+export type EntryNode = Node<CsmNodeProps, 'entry-node'>;
 
-export type StateNode = Node<{
-    name : string
-}, 'state-node'>;
+export type StateNode = Node<CsmNodeProps, 'state-node'>;
 
-export type StateMachineNode = Node<{
-    name : string
-}, 'state-machine-node'>;
+export type StateMachineNode = Node<CsmNodeProps, 'state-machine-node'>;
 
-export type ExitNode = Node<{
-    name: string
-}, 'exit-node'>;
+export type ExitNode = Node<CsmNodeProps, 'exit-node'>;
 
 
 // Node Types with classes. Unsure if needed
