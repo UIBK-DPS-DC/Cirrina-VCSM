e5328d19249299b777578fdbdb42067ac018809a
Lukas Eisenstecken <lukas.eisenstecken@student.uibk.ac.at>
2024-07-15 18:18:04 +0200

    sidebar functionality and service updates

--- src/components/flow.tsx
+++ src/components/flow.tsx
@@ -177,6 +177,37 @@ export default function Flow() {
             setShowSidebar(false);
         },[])
 
+    const onFormSubmit = useCallback((event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (!selectedNode) return;
+
+        const formElements = event.currentTarget.elements as typeof event.currentTarget.elements & {
+            name: HTMLInputElement
+        };
+
+        const newName = formElements.name.value;
+        const oldName = stateOrStateMachineService.getName(selectedNode.data);
+
+        if (!stateOrStateMachineService.isNameUnique(newName) && newName !== oldName) {
+            console.error(`StateOrStateMachine name ${newName} already exists!`);
+            return;
+        }
+
+        if (newName !== oldName) {
+            const newNodes = nodes.map(node => {
+                if (node.id === selectedNode.id) {
+                    const newData = stateOrStateMachineService.setName(newName, node.data);
+                    return { ...node, data: newData };
+                }
+                return node;
+            });
+
+            stateOrStateMachineService.unregisterName(oldName);
+            stateOrStateMachineService.registerName(newName);
+            setNodes(newNodes);
+        }
+    }, [nodes, setNodes, selectedNode, stateOrStateMachineService]);
+
 
 
 
@@ -204,12 +235,13 @@ export default function Flow() {
             </ReactFlow>
             {showSidebar && selectedNode && (
                 <div className = "node-form">
-                    <form>
-                        <h3>Hi mom! It's me {
-                            isState(selectedNode.data) ? selectedNode.data.state.name :
-                                isStateMachine(selectedNode.data) ? selectedNode.data.stateMachine.name :
-                                    selectedNode.data.name
-                        }!</h3>
+                    <form onSubmit={onFormSubmit}>
+                        <h3>Hi mom! It's me {stateOrStateMachineService.getName(selectedNode.data)}!</h3>
+                        <label htmlFor="name" >Name: </label>
+                        <input type = "text" id="name" name = "name" defaultValue={stateOrStateMachineService.getName(selectedNode.data)}
+                        />
+                        <button type={"submit"}>Save Changes</button>
+
                     </form>
 
                 </div>
