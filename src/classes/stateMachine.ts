660aaf65f440552a447b94ec58dd15416d60c8e1
Lukas Geisler csaz8605 <Lukas.Geisler@student.uibk.ac.at>
2024-07-27 11:34:19 +0200

    feat: extended stateMachine.ts to match csml standard

--- src/classes/stateMachine.ts
+++ src/classes/stateMachine.ts
@@ -1,12 +1,16 @@
 import StateOrStateMachine from "./stateOrStateMachine.ts"
+import {Context} from "../types.ts";
+import Action from "./action.tsx";
 
 export default class StateMachine extends StateOrStateMachine {
-    // Static Fields
 
-
-
-    //Object Fields
-    _name: string
+    private _name: string
+    private _states: StateOrStateMachine[] = [];
+    private _localContext: Context[] = [];
+    private _persistentContext: Context[] = [];
+    private _guards: string[] = [];
+    private _actions: Action[] = [];
+    private _abstract = false
 
     public constructor(name: string) {
         super()
@@ -21,7 +25,52 @@ export default class StateMachine extends StateOrStateMachine {
         this._name = name
     }
 
+    get states(): StateOrStateMachine[] {
+        return this._states;
+    }
 
+    set states(value: StateOrStateMachine[]) {
+        this._states = value;
+    }
 
+    get localContext(): Context[] {
+        return this._localContext;
+    }
+
+    set localContext(value: Context[]) {
+        this._localContext = value;
+    }
+
+    get persistentContext(): Context[] {
+        return this._persistentContext;
+    }
+
+    set persistentContext(value: Context[]) {
+        this._persistentContext = value;
+    }
+
+    get guards(): string[] {
+        return this._guards;
+    }
+
+    set guards(value: string[]) {
+        this._guards = value;
+    }
+
+    get actions(): Action[] {
+        return this._actions;
+    }
+
+    set actions(value: Action[]) {
+        this._actions = value;
+    }
+
+    get abstract(): boolean {
+        return this._abstract;
+    }
+
+    set abstract(value: boolean) {
+        this._abstract = value;
+    }
 
 }
