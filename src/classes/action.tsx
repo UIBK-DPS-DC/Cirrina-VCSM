26a04d823db9e7bb42c1b98b4b3e1bc11877de88
Lukas Eisenstecken <lukas.eisenstecken.bz@gmail.com>
2024-07-31 22:13:23 +0200

    chore: added todos

--- src/classes/action.tsx
+++ src/classes/action.tsx
@@ -56,6 +56,7 @@ export default class Action {
                     event: ""
                 }
             }
+            //TODO: OTHER TYPES
             default: {
                 return {}
             }
--- src/components/nodeInfoForm.tsx
+++ src/components/nodeInfoForm.tsx
@@ -205,6 +205,7 @@ export default function NodeInfoForm() {
                     </div>
                 )
             }
+            // TODO: Other Types.
             default : {
                 return <></>
             }
