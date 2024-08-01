503819f0ac09c9e9aaba661723428cffe3076d60
Lukas Eisenstecken <lukas.eisenstecken.bz@gmail.com>
2024-08-01 18:58:26 +0200

    feat: added first tests that interact with the dom directly

--- src/components/Nodes/stateNode.tsx
+++ src/components/Nodes/stateNode.tsx
@@ -14,6 +14,7 @@ export function StateNode ({data}: NodeProps<StateNode>) {
 
     const renderAssociatedEvents = () => {
 
+        // TODO: Style this
         return(
             <div className="raised-event-display">
                 <h4>Raised Events: </h4>
