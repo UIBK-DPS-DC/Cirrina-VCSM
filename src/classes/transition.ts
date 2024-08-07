767eddd9b95b5f458976cf16e85add4a99f96c16
Lukas Eisenstecken <lukas.eisenstecken.bz@gmail.com>
2024-08-07 17:11:36 +0200

    feat: extended transition class

--- src/classes/transition.ts
+++ src/classes/transition.ts
@@ -1,11 +1,20 @@
+import Action from "./action.tsx";
 
 export default class Transition {
     private source : string
     private target : string
+    private _guards : string[]
+    private _actions : Action[]
+    private _else : string []
+    private _event : string
 
     public constructor (sourceState: string, targetState: string){
         this.source = sourceState
         this.target = targetState
+        this._guards = []
+        this._actions = []
+        this._else = []
+        this._event = ""
     }
 
 
@@ -25,11 +34,36 @@ export default class Transition {
         return this.target
     }
 
-    
 
+    get guards(): string[] {
+        return this._guards;
+    }
 
-    
+    set guards(value: string[]) {
+        this._guards = value;
+    }
 
+    get actions(): Action[] {
+        return this._actions;
+    }
 
+    set actions(value: Action[]) {
+        this._actions = value;
+    }
 
+    get else(): string[] {
+        return this._else;
+    }
+
+    set else(value: string[]) {
+        this._else = value;
+    }
+
+    get event(): string {
+        return this._event;
+    }
+
+    set event(value: string) {
+        this._event = value;
+    }
 }
