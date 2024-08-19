e6ea8896123d00392a11f5b80044e2d39adcef35
Lukas Eisenstecken <lukas.eisenstecken@student.uibk.ac.at>
2024-08-19 22:59:58 +0200

    chore: updated tests for new guard class

--- test/guardService.test.tsx
+++ test/guardService.test.tsx
@@ -1,4 +1,5 @@
 import GuardService from "../src/services/guardService";
+import Guard from "../src/classes/guard";
 
 describe('GuardService', () => {
     let guardService: GuardService;
@@ -14,26 +15,30 @@ describe('GuardService', () => {
     });
 
     test('isNameUnique should return false for a non-unique name', () => {
-        guardService.registerGuard('nonUniqueGuard', 'expression');
+        const newGuard = new Guard('expression', 'nonUniqueGuard')
+        guardService.registerGuard(newGuard);
         expect(guardService.isNameUnique('nonUniqueGuard')).toBe(false);
     });
 
     // Test registerGuard method
     test('registerGuard should register a guard with a unique name', () => {
-        const result = guardService.registerGuard('newGuard', 'someExpression');
+        const newGuard = new Guard('someExpression', 'newGuard');
+        const result = guardService.registerGuard(newGuard);
         expect(result).toBe(true);
         expect(guardService.isNameUnique('newGuard')).toBe(false); // Guard should now be registered
     });
 
     test('registerGuard should not register a guard with a duplicate name', () => {
-        guardService.registerGuard('duplicateGuard', 'firstExpression');
-        const result = guardService.registerGuard('duplicateGuard', 'secondExpression');
+        const newGuard = new Guard('firstExpression', 'duplicateGuard')
+        guardService.registerGuard(newGuard);
+        const result = guardService.registerGuard(newGuard);
         expect(result).toBe(false);
     });
 
     // Test unregisterGuard method
     test('unregisterGuard should remove a registered guard', () => {
-        guardService.registerGuard('guardToRemove', 'expression');
+        const newGuard = new Guard('expression', 'guardToRemove')
+        guardService.registerGuard(newGuard);
         guardService.unregisterGuard('guardToRemove');
         expect(guardService.isNameUnique('guardToRemove')).toBe(true); // Guard should be unregistered
     });
@@ -46,13 +51,16 @@ describe('GuardService', () => {
 
     // Test linkNameToGuard method
     test('linkNameToGuard should link a name to a new expression', () => {
-        guardService.linkNameToGuard('linkedGuard', 'initialExpression');
+        const newGuard = new Guard('initialExpression', "linkedGuard")
+        guardService.linkGuard(newGuard);
         expect(guardService.isNameUnique('linkedGuard')).toBe(false); // Guard should now be registered
     });
 
     test('linkNameToGuard should update the expression of an existing guard', () => {
-        guardService.linkNameToGuard('guardToUpdate', 'initialExpression');
-        guardService.linkNameToGuard('guardToUpdate', 'updatedExpression');
+        const newGuard = new Guard('initialExpression','guardToUpdate')
+        const updatedGuard = new Guard('updatedExpression','guardToUpdate')
+        guardService.linkGuard(newGuard);
+        guardService.linkGuard(updatedGuard);
 
         expect(guardService.isNameUnique('guardToUpdate')).toBe(false);
         expect(guardService.getGuardExpression("guardToUpdate")).toEqual("updatedExpression")
@@ -64,8 +72,10 @@ describe('GuardService', () => {
     });
 
     test('getAllGuardNames should return an array of all registered guard names', () => {
-        guardService.registerGuard('guard1', 'expression1');
-        guardService.registerGuard('guard2', 'expression2');
+        const guard1 = new Guard("expression1", "guard1")
+        const guard2 = new Guard("expression2", "guard2")
+        guardService.registerGuard(guard1);
+        guardService.registerGuard(guard2);
 
         const guardNames = guardService.getAllGuardNames();
         expect(guardNames).toContain('guard1');
@@ -75,7 +85,8 @@ describe('GuardService', () => {
 
     // Tests for getGuardExpression method
     test('getGuardExpression should return the correct expression for a registered guard', () => {
-        guardService.registerGuard('guard1', 'expression1');
+        const newGuard = new Guard("expression1", "guard1")
+        guardService.registerGuard(newGuard);
 
         const expression = guardService.getGuardExpression('guard1');
         expect(expression).toBe('expression1');
