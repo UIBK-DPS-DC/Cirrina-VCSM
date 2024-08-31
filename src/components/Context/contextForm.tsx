caac5cd87cc479c579f196c47ea5221c32916db1
Lukas Eisenstecken <lukas.eisenstecken.bz@gmail.com>
2024-09-01 01:08:29 +0200

    feat: added validation to context form

--- src/components/Context/contextForm.tsx
+++ src/components/Context/contextForm.tsx
@@ -13,7 +13,8 @@ export default function ContextForm(props: {variable: ContextVariable | undefine
     const EXPRESSION_FIELD_NAME = "expression";
     const CONTEXT_TYPE_FIELD_NAME = "contextType";
 
-    const buttonText = () => props.variable ? "Update Variable" : "Create Variable";
+
+
     const oldName = props.variable ? props.variable.name : null;
 
 
@@ -25,28 +26,37 @@ export default function ContextForm(props: {variable: ContextVariable | undefine
     selectedNode} = context;
 
 
+    const [variableNameInput, setVariableNameInput] = useState<string>("");
+    const [variableValueInput, setVariableValueInput] = useState<string>("");
+    const [contextTypeValue, setContextTypeValue] = useState<string>("");
 
     const [formIsValid, setFormIsValid] = useState<boolean>(false);
     const [variableNameIsValid, setVariableNameIsValid] = useState<boolean>(false);
     const [expressionIsValid, setExpressionIsValid] = useState<boolean>(false);
     const [contextTypeIsValid, setContextTypeIsValid] = useState<boolean>(true);
 
+    const buttonText = () => props.variable ? "Update Variable" : "Create Variable";
+    const invalidNameText = () => variableNameInput? `A variable with name "${variableNameInput}" already exists` : "Name cannot be empty";
 
-    useEffect(() => {
+
+        useEffect(() => {
         setFormIsValid(variableNameIsValid && expressionIsValid && contextTypeIsValid);
     }, [variableNameIsValid, expressionIsValid, contextTypeIsValid]);
 
 
     const validateVariableName = useCallback((name: string) => {
+        if(!name){
+            return false;
+        }
         if ((props.variable && props.variable.name !== name) || !props.variable) {
             return contextService.isContextNameUnique(name);
         }
         return true;
     }, [props.variable, contextService]);
 
-    const validateExpression = useCallback((_: string) => {
+    const validateExpression = useCallback((e: string) => {
         // TODO: Logic to validate expression.
-        return true;
+        return !!e
     }, []);
 
     const validateContextType = useCallback((contextType: string) => {
@@ -57,9 +67,6 @@ export default function ContextForm(props: {variable: ContextVariable | undefine
         return variableNameIsValid && expressionIsValid && contextTypeIsValid;
     }, [variableNameIsValid, expressionIsValid, contextTypeIsValid]);
 
-    const [variableNameInput, setVariableNameInput] = useState<string>("");
-    const [variableValueInput, setVariableValueInput] = useState<string>("");
-    const [contextTypeValue, setContextTypeValue] = useState<string>("");
 
     const onVariableNameInputChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value;
@@ -136,7 +143,19 @@ export default function ContextForm(props: {variable: ContextVariable | undefine
         <Form onSubmit={handleSubmit} className={"no"}>
             <Form.Group>
                 <Form.Label>Name</Form.Label>
-                <Form.Control type={"text"} name={VARIABLE_NAME_FIELD_NAME} placeholder={"Variable Name"} value={variableNameInput} onChange={onVariableNameInputChange} required isValid={variableNameIsValid}/>
+                <Form.Control
+                    type={"text"}
+                    name={VARIABLE_NAME_FIELD_NAME}
+                    placeholder={"Variable Name"}
+                    value={variableNameInput}
+                    onChange={onVariableNameInputChange}
+                    required
+                    isValid={variableNameIsValid}
+                    isInvalid={!variableNameIsValid}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {invalidNameText()}
+                </Form.Control.Feedback>
                 <Form.Text className={"text-muted"}>
                     The new Variable's Name
                 </Form.Text>
@@ -144,7 +163,19 @@ export default function ContextForm(props: {variable: ContextVariable | undefine
 
             <Form.Group>
                 <Form.Label>Value</Form.Label>
-                <Form.Control type={"text"} name={EXPRESSION_FIELD_NAME} placeholder={"Value"} value={variableValueInput} onChange={onVariableValueInputChange} required isValid={expressionIsValid}/>
+                <Form.Control
+                    type={"text"}
+                    name={EXPRESSION_FIELD_NAME}
+                    placeholder={"Value"}
+                    value={variableValueInput}
+                    onChange={onVariableValueInputChange}
+                    required
+                    isValid={expressionIsValid}
+                    isInvalid={!expressionIsValid}
+                />
+                <Form.Control.Feedback type="invalid">
+                    Please provide a valid expression.
+                </Form.Control.Feedback>
                 <Form.Text className={"text-muted"}>
                     The variable's value. Must be an <a href={EXPRESSION_INFO_LINK} target={"_blank"}>Expression</a>.
                 </Form.Text>
@@ -152,16 +183,27 @@ export default function ContextForm(props: {variable: ContextVariable | undefine
 
             <Form.Group>
                 <Form.Label>Context Type</Form.Label>
-                <Form.Select name={CONTEXT_TYPE_FIELD_NAME} value={contextTypeValue} onChange={onContextTypeValueChange} isValid={contextTypeIsValid}>
+                <Form.Select
+                    name={CONTEXT_TYPE_FIELD_NAME}
+                    value={contextTypeValue}
+                    onChange={onContextTypeValueChange}
+                    isValid={contextTypeIsValid}
+                    isInvalid={!contextTypeIsValid}
+                >
                     {renderEnumAsOptions(ContextType)}
                 </Form.Select>
+                <Form.Control.Feedback type="invalid">
+                    Please select a valid context type.
+                </Form.Control.Feedback>
                 <Form.Text className={"text-muted"}>
                     The Context Type of the variable affects its visibility. See the <a href={SPECIFICATIONS_LINK} target={"_blank"}>Specification</a>.
                 </Form.Text>
             </Form.Group>
-            <Button variant="primary" type="submit" disabled={!formIsValid}>
+
+            <Button variant="primary" type="submit" disabled={!formIsValid} className={!formIsValid ? 'disabled' : ''}>
                 {buttonText()}
             </Button>
         </Form>
+
     );
 }
