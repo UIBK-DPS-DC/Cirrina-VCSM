c64c0c5348f56b390d2400cde3d4b877192e26e3
Lukas Eisenstecken <lukas.eisenstecken.bz@gmail.com>
2024-09-18 19:12:33 +0200

    feat: extented onTransitionForm

--- src/components/visualEditor/transitionInfoForm.tsx
+++ src/components/visualEditor/transitionInfoForm.tsx
@@ -1,5 +1,5 @@
-import React, {useContext, useEffect, useState} from "react";
-import {ReactFlowContext} from "../../utils.tsx";
+import React, {useContext, useEffect, useRef, useState} from "react";
+import {ReactFlowContext, renderEnumAsOptions} from "../../utils.tsx";
 import {ReactFlowContextProps} from "../../types.ts";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import {
@@ -7,7 +7,7 @@ import {
     Col,
     Container,
     Form,
-    InputGroup,
+    InputGroup, ModalBody,
     OffcanvasBody,
     OffcanvasHeader,
     OffcanvasTitle,
@@ -16,17 +16,31 @@ import {
 import Guard from "../../classes/guard.tsx";
 import GuardDisplay from "../Guard/guardDisplay.tsx";
 import Action from "../../classes/action.ts";
-import SelectEventsModal from "../Event/selectEventsModal.tsx";
 import Event from "../../classes/event.ts";
 import SelectSingleEventModal from "../Event/selectSingleEventModal.tsx";
+import {ActionType} from "../../enums.ts";
+import InvokeActionForm from "./ActionForms/invokeActionForm.tsx";
+import CreateActionForm from "./ActionForms/createActionForm.tsx";
+import AssignActionForm from "./ActionForms/assignActionForm.tsx";
+import RaiseEventActionForm from "./ActionForms/raiseEventActionForm.tsx";
+import TimeoutActionForm from "./ActionForms/timeoutActionForm.tsx";
+import TimeoutResetActionForm from "./ActionForms/timeoutResetActionForm.tsx";
+import MatchActionForm from "./ActionForms/matchActionForm.tsx";
+import Modal from "react-bootstrap/Modal";
 
 
+let idCounter = 0
 
 
 export default function TransitionInfoForm() {
     const context = useContext(ReactFlowContext) as ReactFlowContextProps;
     const {selectedEdge, showSidebar, setShowSidebar,
     eventService, actionService} = context
+
+    const instanceId = useRef(++idCounter).current;
+    let formCount = 0
+
+    const formId = `${instanceId}-${formCount++}`
 
 
     const [guardInput, setGuardInput] = useState<string>("")
@@ -36,6 +50,43 @@ export default function TransitionInfoForm() {
     const [onEvent, setOnEvent] = useState<Event | undefined>()
 
 
+
+    const [invokeAction, setInvokeAction] = useState<Action[]>([]);
+    const [createAction, setCreateAction] = useState<Action[]>([]);
+    const [assignAction, setAssignAction] = useState<Action[]>([]);
+    const [raiseEventAction, setRaiseEventAction] = useState<Action[]>([]);
+    const [timeoutAction, setTimeoutAction] = useState<Action[]>([]);
+    const [timeOutResetAction, setTimeoutResetAction] = useState<Action[]>([]);
+    const [matchAction, setMatchAction] = useState<Action[]>([]);
+    const [selectedActionType, setSelectedActionType] = useState<string>(ActionType.CREATE);
+
+
+    const [showActionModal, setShowActionModal] = useState<boolean>(false)
+
+    const handleShow = () => setShowActionModal(true)
+    const handleClose = () => setShowActionModal(false)
+
+
+    const renderActionForm = () => {
+        switch (selectedActionType) {
+            case ActionType.INVOKE:
+                return <InvokeActionForm action={invokeAction[0]} setActions={setInvokeAction} noCategorySelect={true} dontAddToState={true} onSubmit={handleClose}/>;
+            case ActionType.CREATE:
+                return <CreateActionForm action={createAction[0]} setActions={setCreateAction} noCategorySelect={true} dontAddToState={true} onSubmit={handleClose}  />;
+            case ActionType.ASSIGN:
+                return <AssignActionForm action={assignAction[0]} setActions={ setAssignAction} noCategorySelect={true} dontAddToState={true} onSubmit={handleClose} />;
+            case ActionType.RAISE_EVENT:
+                return <RaiseEventActionForm action={raiseEventAction[0]} setActions={setRaiseEventAction} noCategorySelect={true} onSubmit={handleClose}/>;
+            case ActionType.TIMEOUT:
+                return <TimeoutActionForm action={timeoutAction[0]} setActions={setTimeoutAction} noCategorySelect={true} onSubmit={handleClose}/>;
+            case ActionType.TIMEOUT_RESET:
+                return <TimeoutResetActionForm action={timeOutResetAction[0]} setActions={setTimeoutResetAction} noCategorySelect={true} onSubmit={handleClose}/>;
+            case ActionType.MATCH:
+                return <MatchActionForm action={matchAction[0]} setActions={setMatchAction}  dontAddToState={true} onSubmit={handleClose}/>;
+            default:
+                return null;
+        }
+    };
 
 
     const offcanvasTitle = () => selectedEdge?.data?.transition ?
@@ -63,6 +114,10 @@ export default function TransitionInfoForm() {
     const onGuardInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setGuardInput(event.target.value)
     }
+
+    const onSelectedActionTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedActionType(event.target.value);
+    };
 
     const onAddGuardClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault()
@@ -103,7 +158,7 @@ export default function TransitionInfoForm() {
                     </OffcanvasHeader>
 
                     <OffcanvasBody>
-                        <Form>
+                        <Form id={formId}>
 
                             <Form.Group as={Row} className={"mb-3"}>
                                 <Form.Label column sm={"2"}>Source State: </Form.Label>
@@ -164,6 +219,35 @@ export default function TransitionInfoForm() {
                             </Container>
                         )}
 
+                        <Container className={"mb-3"}>
+                            <Button onClick={handleShow}>
+                                Add Action
+                                <i className="bi bi-plus-circle"></i>
+                            </Button>
+
+                            <Modal show={showActionModal} size={"lg"} onHide={handleClose}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Add Action</Modal.Title>
+                                </Modal.Header>
+
+                                <ModalBody>
+                                    <Row className={"mb-3"}>
+                                        <Form.Label column sm={"4"}>Action type:</Form.Label>
+                                        <Col sm={8}>
+                                            <Form.Select value={selectedActionType} onChange={onSelectedActionTypeChange}>
+                                                {renderEnumAsOptions(ActionType)}
+                                            </Form.Select>
+                                        </Col>
+                                    </Row>
+                                    {renderActionForm()}
+                                </ModalBody>
+
+                                <Modal.Footer>
+                                    <Button variant="secondary" onClick={handleClose}>Close</Button>
+                                </Modal.Footer>
+
+                            </Modal>
+                        </Container>
 
 
                     </OffcanvasBody>
