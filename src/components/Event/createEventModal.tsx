54ce462cd4e37f333b0f7fd18b6ced4c385e506c
Lukas Eisenstecken <lukas.eisenstecken.bz@gmail.com>
2024-10-04 18:36:48 +0200

    feat: added darkmode toggle to createEventModal

--- src/components/Event/createEventModal.tsx
+++ src/components/Event/createEventModal.tsx
@@ -1,12 +1,16 @@
 import Modal from "react-bootstrap/Modal";
 import Event from "../../classes/event.ts";
 import {Button, Container, ModalBody} from "react-bootstrap";
-import React, {Dispatch, SetStateAction} from "react";
+import React, {Dispatch, SetStateAction, useContext} from "react";
 import CreateEventForm from "./createEventForm.tsx";
 import ContextVariable from "../../classes/contextVariable.tsx";
+import {ReactFlowContext} from "../../utils.tsx";
+import {ReactFlowContextProps} from "../../types.ts";
 
 export default function CreateEventModal(props: {event: Event | undefined, onSubmit: (updatedEvent: Event) => void, noCloseCascade?: Boolean, setVars?: Dispatch<SetStateAction<ContextVariable[]>>, buttonVariant? :string, buttonSize? : "sm" | "lg"}) {
 
+    const context = useContext(ReactFlowContext) as ReactFlowContextProps;
+    const {darkMode} = context
     const [show, setShow] = React.useState(false);
 
     const handleShow = () => setShow(true)
@@ -29,7 +33,7 @@ export default function CreateEventModal(props: {event: Event | undefined, onSub
             <Button onClick={handleShow} variant={props.buttonVariant ? props.buttonVariant : "primary"} size={props.buttonSize ? props.buttonSize : undefined}>
                 {buttonText()}
             </Button>
-            <Modal show={show} onHide={handleClose} backdrop={"static"} data-bs-theme="dark">
+            <Modal show={show} onHide={handleClose} backdrop={"static"} data-bs-theme={darkMode ? "dark" : "light"}>
                 <Modal.Header closeButton style={{color: "#ffffff"}}>
                     <Modal.Title>{modalTitle()}</Modal.Title>
                 </Modal.Header
