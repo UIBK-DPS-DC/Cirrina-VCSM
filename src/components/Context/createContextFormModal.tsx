68719c632cb7a1b15811d352da4fe232a885bff8
Lukas Eisenstecken <lukas.eisenstecken.bz@gmail.com>
2024-10-04 18:32:18 +0200

    feat: added darkmode toggle to createContextFormModal

--- src/components/Context/createContextFormModal.tsx
+++ src/components/Context/createContextFormModal.tsx
@@ -1,13 +1,17 @@
-import { useState } from 'react';
+import {useContext, useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import ContextVariable from "../../classes/contextVariable.tsx";
 import { Container } from "react-bootstrap";
 import CreateContextForm from "./createContextForm.tsx";
+import {ReactFlowContext} from "../../utils.tsx";
+import {ReactFlowContextProps} from "../../types.ts";
 
 export default function CreateContextFormModal(props: { variable: ContextVariable | undefined, buttonName: string | undefined, onSubmit: ((updatedVariable: ContextVariable) => void) | undefined, noCascadeClose?: boolean, noRegister?: boolean }) {
 
     const [show, setShow] = useState(false);
+    const context = useContext(ReactFlowContext) as ReactFlowContextProps
+    const {darkMode} = context
 
     const handleShow = () => setShow(true);
     const handleClose = () => setShow(false);
@@ -31,7 +35,7 @@ export default function CreateContextFormModal(props: { variable: ContextVariabl
                 {buttonName()}
             </Button>
 
-            <Modal show={show} onHide={handleClose} data-bs-theme="dark" backdrop="static" >
+            <Modal show={show} onHide={handleClose} data-bs-theme={darkMode ? "dark" : "light"} backdrop="static" >
                 <Modal.Header closeButton style={{color: "#ffffff"}}>
                     <Modal.Title>{modalTitle()}</Modal.Title>
                 </Modal.Header>
